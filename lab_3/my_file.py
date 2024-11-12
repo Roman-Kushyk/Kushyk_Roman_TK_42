1f4facaee2db700c21852839868de126f81dacd6
Roman Kushyk <romanviktorowytsh@gmail.com>
2024-11-12 19:39:17 +0000

    Change

--- lab_3/my_file.py
+++ lab_3/my_file.py
@@ -13,6 +13,7 @@ class PhoneWithAttributes:
         self.made_in = made_in
         self._reserved_memory:float = round(0.1 * int(ram), 2) # Це є захищений атрибут
         self.__privat_message = "Всі телефони зроблені в Китаї"
+        self._video_calls = 0
 
     @property
     def get_availabl_memory(self):
@@ -21,3 +22,19 @@ class PhoneWithAttributes:
     @property
     def secret(self):
         return f"Ось наш секрет: {self.__privat_message}"
+    def call(self, phone_number):
+        print(f"Набираємо номер телефона: {phone_number}")
+        self.ram -= 0.5
+        print(f"Програма опрацювання відеодзвінка, тепер доступно : {self.ram} оперативки")
+        self._video_calls += 1
+        return True
+    
+    def get_call(self):
+        print("Отримали виклик від когось")
+
+    def stop_call(self):
+        if self._video_calls > 0:
+            self.ram += 0.5
+            self._video_calls -= 1
+            return True
+        return False
